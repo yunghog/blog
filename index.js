5bc0fa170f9539d479cb95fe5f60d6bc01b94e42
yunghog <samarthahm@gmail.com>
2020-07-08 16:03:29 +0530

    index page dynamic content

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var app = express();
 var bodyParser = require('body-parser');
 var router = express.Router();
 var nodemailer = require('nodemailer');
+var MongoClient = require('mongodb').MongoClient;
 
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
 var transporter = nodemailer.createTransport({
@@ -11,8 +12,8 @@ var transporter = nodemailer.createTransport({
   secure: false,
   requireTLS: true,
   auth: {
-    user: 'samarthahm@gmail.com',
-    pass: 'wwe9481504454'
+    user: 'creedthoughts66@gmail.com',
+    pass: '#yungh0gbeat5'
   }
 });
 var path = __dirname + '/views/';
@@ -32,24 +33,30 @@ router.get('/contact',function(req, res){
   res.sendFile(path + 'contact.html');
 });
 
-app.post('/mailingList', urlencodedParser, function (req, res) {
-   response = {
-      email:req.body.email
-   };
+app.post('/mailingList', urlencodedParser,(req, res) => {
+   response = {email:req.body.email};
    mailOptions={
      from: 'Creed Thoughts',
      to: req.body.email,
-  subject: 'Subscription to Creed Thoughts',
-  text: 'You hace successfully subscribed to Creed Thoughts'
-};
-   transporter.sendMail(mailOptions, function(error, info){
-  if (error) {
-    console.log(error);
-  } else {
-    console.log('Email sent: ' + info.response);
-  }
-});
-   console.log(response);
+     subject: 'Subscription to Creed Thoughts',
+     text: 'You hace successfully subscribed to Creed Thoughts'};
+    transporter.sendMail(mailOptions, function(error, info){
+      if (error) {
+        console.log(error);
+      } else {
+        console.log('Email sent: ' + info.response);
+      }
+    });
+    MongoClient.connect('mongodb://127.0.0.1:27017', {useUnifiedTopology: true}, (err, client) => {
+      if (err) return console.error(err);
+      console.log('Connected to Database');
+      const db = client.db('creedthoughts');
+      const subsCollection = db.collection('subscribers');
+      subsCollection.count({},(err,n)=>{
+        n=n+1;
+        subsCollection.insert({"_id":n ,"email":req.body.email});
+      });
+    });
    res.redirect('/subscription');
 });
 app.listen(3030, function () {
